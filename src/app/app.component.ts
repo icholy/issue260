1e089e3bf78f0ccd4093eb840c969fe98288639d
Ilia Choly <icholy@accipiterradar.com>
2020-04-01 13:06:52 -0400

    add media-stream-library

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 
+import { pipelines } from 'media-stream-library/dist/esm/index.browser.js'
+
+// no error unless the pipeline class is referenced
+pipelines.Html5VideoPipeline;
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
